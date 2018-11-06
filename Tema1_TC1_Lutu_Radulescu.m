7c5cc600f6ab79a8dd5ea2c5aa382c1bab0f4383
RadulescuAndrei <44034149+RadulescuAndrei@users.noreply.github.com>
2018-11-06 19:17:24 +0200

    Add files via upload

---
+++ Tema1_TC1_Lutu_Radulescu.m
@@ -0,0 +1,11 @@
+T = 2; 
+F = 1/T; 
+x = 0:0.002:5; 
+a = square(2*pi*F*x, 25);a = red(a); 
+subplot(3,1,1),plot(x,a,'.-'), grid, title('2ms');
+x = 0:0.02:5;
+a = square(2*pi*F*x, 25);a = red(a);
+subplot(3,1,2),plot(x,a,'.-'),grid, title('20ms');
+x = 0:0.2:5;
+a = square(2*pi*F*x, 25);a = red(a);
+subplot(3,1,3),plot(x,a, '.-'),grid, title('200ms');
